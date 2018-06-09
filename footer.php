09343dcff1f84b496c8b6d5519a82d0b667c92ef
Korpi <36895821+3PMX@users.noreply.github.com>
2018-06-09 14:18:11 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
-</body>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Footer</title>
+	</head>
+	<body>
+		<hr>
+	<p>Projekt wykonali: Piotr Korpas, Mateusz Kawiak, Piotr Kiczmach</p>
+	</body>
 </html>
