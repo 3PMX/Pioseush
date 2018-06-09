cc53bed9a9893b7745c362d9e17ddf7e73216496
Korpi <36895821+3PMX@users.noreply.github.com>
2018-06-09 14:20:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
     include_once 'header.php';
 ?>
 <section class="mainkontener">
-    <div class="maincontent"> <h2>Pioseush™ - Twoje zamówienia w jednym miejscu. </h2>
-    <p> Kontakt: pioseushteam@gmail.com </p>
-     </div>
 
+    <div class="maincontent"> <h2>Pioseush™ - Twoje zamówienia w jednym miejscu. </h2>
+    <p>Kontakt z naszym biurem: <a href="mailto:biuro.3pmx@int.pl">biuro.3pmx@int.pl</a></p>
+    </div>
+    
 </section>
 
 <?php
